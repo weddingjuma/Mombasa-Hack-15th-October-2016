6f19da94dcc1fef41b4c816ad88040e3718f922c
janikibichi <janikibichi@gmail.com>
2016-10-15 13:28:05 +0300

    secondary changes

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask import request
+
 app = Flask('the-box-hack')
-books = {'name':'Mombasa raha'}
+books = [{'name':'Mombasa raha'}]
 
 @app.route('/api/category/books/', methods=['GET','POST'])
 def book_api():
-    return books
+	if request.method == 'GET':
+        return jsonify(books)
+    else:
+    	name = request.values.get('name', None)
+    
 
 if __name__ = '__main__':
     app.run()
