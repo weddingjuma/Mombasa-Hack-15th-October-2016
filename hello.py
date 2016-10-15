29ab6f0cb415d5111603689065b2d01dd3834c98
Rodgers-M <mkishagha@gmail.com>
2016-10-15 14:49:53 +0300

    add some functionality to the app to post resources

--- hello.py
+++ hello.py
@@ -2,17 +2,36 @@ from flask import Flask
 from flask import jsonify, make_response
 app = Flask('rodgy-library')
 
-books = {
+books = [{
 	'name': 'Harry Potter and the priosoner',
 	'auther': 'jk. Rowling',
 	'category': 'Magic/Fiction',
 	'country' : 'Kenya',
 	'id': '4325345234'
-}
+}]
 
 @app.route('/api/category/books/', methods=['GET', 'POST'])
 def book_api():
-	return jsonify(book)
+	resp = ''
+	if request.method == 'GET':
+	   resp = jsonify(books)
+       
+    else:
+    	name     = request.values.get('name', None)
+        author   = request.values.get('author', None)
+        category = request.values.get('category', None)
+        id_      = request.values.get('id', None)
+
+        new_book = {
+          'name': name,
+          'author': author,
+          'category': category,
+          'id': id_
+        }
+        books.append(new_book)
+    	resp = jsonify({'OK': 'Book added'})
+
+	return resp
 
 if __name__ == '__main__':
 	app.run()
