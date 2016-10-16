2cf122c9030dcd30abf87bd4973421b001b4fa84
Rodgers-M <mkishagha@gmail.com>
2016-10-16 09:25:43 +0300

    Enable debug mode

--- hello.py
+++ hello.py
@@ -1,23 +1,22 @@
-from flask import Flask
+from flask import Flask, request
 from flask import jsonify, make_response
 app = Flask('rodgy-library')
 
 books = [{
-	'name': 'Harry Potter and the priosoner',
-	'auther': 'jk. Rowling',
-	'category': 'Magic/Fiction',
-	'country' : 'Kenya',
-	'id': '4325345234'
+  'name': 'Harry Potter and the priosoner',
+  'auther': 'jk. Rowling',
+  'category': 'Magic/Fiction',
+  'country' : 'Kenya',
+  'id': '4325345234'
 }]
 
 @app.route('/api/category/books/', methods=['GET', 'POST'])
 def book_api():
-	resp = ''
-	if request.method == 'GET':
-	   resp = jsonify(books)
-       
-    else:
-    	name     = request.values.get('name', None)
+    resp = ''
+    if request.method == 'GET':
+        resp = jsonify(books)
+    elif request.method == 'POST':
+        name     = request.values.get('name', None)
         author   = request.values.get('author', None)
         category = request.values.get('category', None)
         id_      = request.values.get('id', None)
@@ -29,9 +28,11 @@ def book_api():
           'id': id_
         }
         books.append(new_book)
-    	resp = jsonify({'OK': 'Book added'})
+        resp = "book added succesfully"
 
-	return resp
+    return resp
 
 if __name__ == '__main__':
-	app.run()
+  app.debug=True
+  app.run()
+  app.run(debug=True)
